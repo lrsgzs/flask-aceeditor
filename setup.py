14b284522731b8a8ad45157b54c0a3a725ed7ca5
lrsgzs <liurongshuo2022@outlook.com>
2022-08-12 15:39:20 +0800

    setup.py is new

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
-from distutils.core import setup
+import os
+
+from setuptools import setup
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(basedir, 'README.md'), encoding='utf-8') as f:
+    long_des = f.read()
 
 setup(
     name='Flask-AceEditor',
     version='1.0.0',
     packages=['flask-aceeditor'],
-    url='',
+    url='https://github.com/lrsgzs/flask-aceeitor',
     license='MIT',
     author='LRS',
     author_email='liurongshuo2022@outlook.com',
-    description='This is AceEditor on flask.'
+    description='This is AceEditor on flask.',
+    long_description=long_des,
+    keywords="flask extension lrspackages",
+    long_description_content_type='text/markdown',
+    install_requires=['flask'],
+    extras_require={},
+    include_package_data=True,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
 )
