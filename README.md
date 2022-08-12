c5a7c2e77c4261eba6c15976a6bd78014cf00a1e
lrsgzs <liurongshuo2022@outlook.com>
2022-08-12 16:33:26 +0800

    README.md is updata!!!

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
 # flask-aceeditor
 
-因为这个库(flask-aceeditor)，所以可以实现ace编译器得调用了！
+## 因为这个库(flask-aceeditor)，所以在使用ace编辑器能很方便地调用了！
+
+### 安装
 
 ```shell
 $ pip install flask-aceeditor
 ```
+
+### 使用
+
+```python
+from flask_aceeditor import *
+...
+aceeditor = AceEditor(app)
+```
+
+#### 加载js文件
+
+```html
+...
+{{ ace.load() }}<!--一定要在使用ace编辑器的代码前面！！！-->
+...
+
+</html>
+```
+
+#### 创建编辑器（有颜色变换按钮）
+
+```html
+...
+{{ ace.create(name="code") }}<!-- name默认是"code" -->
+```
+
+##### 注：提交表单时，字段是你当时创建时传入的参数name
---
+++ flask_aceeditor/__init__.py
@@ -0,0 +1,80 @@
+from flask import *
+
+
+# create a aceeditor class
+class AceEditor(object):
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        if not hasattr(app, "extensions"):
+            app.extensions = {}
+        app.extensions["aceeditor"] = self
+        app.jinja_env.globals["aceeditor"] = self
+        app.jinja_env.globals["ace"] = self
+        acebp = Blueprint('ace', __name__, static_folder="static", static_url_path="/ace" + app.static_url_path)
+        app.register_blueprint(acebp)
+        self.done = (False, False)
+
+    @staticmethod
+    def load():
+        jzs = ["ace.min.js",
+               "mode-python.min.js",
+               "theme-monokai.min.js",
+               "theme-xcode.min.js",
+               "python.min.js",
+               "ext-searchbox.min.js",
+               "ext-language_tools.min.js",
+               "ext-beautify.min.js",
+               "jquery-3.6.0.min.js"
+               ]
+        tp = ('<script src="', '"></script>')
+        jzjbs = []
+        for i in jzs:
+            jzjbs.append(tp[0] + url_for("ace.static", filenamee=i) + tp[1])
+        wq = ""
+        for i in jzjbs:
+            wq = wq + i + "\n"
+        return Markup(wq)
+
+    @staticmethod
+    def create(name="code", code="print('helloworld')"):
+        a = '<pre id="content" style="height:415px"></pre><textarea name="' + name + '" id="' + name + \
+            '''" class="code">''' + code + '''</textarea><button onclick="ghzt()">白天/黑夜</button><script>
+var codek = document.getElementById("''' + name + '''");
+var codes = codek.innerHTML;
+
+var editor = ace.edit("content");
+editor.setTheme('ace/theme/xcode');
+let jsMode = ace.require('ace/mode/python').Mode;
+editor.session.setMode(new jsMode());
+editor.setFontSize(15);
+editor.setValue(codes);
+editor.moveCursorTo(0, 0);
+ace.require("ace/ext/searchbox");
+ace.require("ace/ext/language_tools");
+ace.require("ace/ext/beautify");
+editor.setOptions({
+    enableBasicAutocompletion: true,
+    enableSnippets: true,
+    enableLiveAutocompletion: true
+});
+
+var textarea = $('textarea[name="''' + name + '''"]').hide();
+editor.getSession().on('change', function(){
+  textarea.val(editor.getSession().getValue());
+});
+
+var ztnum = 1;
+function ghzt(){
+    if (ztnum === 1){
+        ztnum = 2;
+        editor.setTheme('ace/theme/monokai');
+    }else{
+        ztnum = 1;
+        editor.setTheme('ace/theme/xcode');
+    }
+}
+</script>'''
+        return Markup(a)
